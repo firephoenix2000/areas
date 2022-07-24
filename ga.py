12fa1136da2cf4510047920464497f6a6adfbab5
yudejun <yudejun2000@outlook.com>
2022-07-24 15:56:03 +0800

    modify some chromosomes select bug

--- ga.py
+++ ga.py
@@ -9,10 +9,12 @@ import random
 from shape import generate_triangle_data, Triangle, Point
 
 CHECK_CHROMOSOME_NUMBER = 200
-SELECTED_NUMBER =20
+SELECTED_NUMBER = 20
 TRIANGLE_POINT_SCOPE=10
 MUTUTAION_PROBABILITY = 0.03
 gene_list = ['a', 'b', 'c', 'd', 'e', 'f', '+', '-', '*']
+symbol_list = ['a', 'b', 'c', 'd', 'e', 'f']
+operator_list = ['+', '-', '*']
 
 
 def genetic_algorithm(epochs=1000):
@@ -32,6 +34,7 @@ def genetic_algorithm(epochs=1000):
 
     for i in range(epochs):
         # 1. select
+        # print('before select  number is', len(tmp1_chromosomes))
         selected = select(tmp1_chromosomes)
 
         # 2. crossover
@@ -41,8 +44,17 @@ def genetic_algorithm(epochs=1000):
         length = len(tmp1_chromosomes)
         x = int(length*MUTUTAION_PROBABILITY)
         for j in range(x):
-            t = random.randint(0,length)
+            t = random.randint(0,length-1)
             tmp1_chromosomes[t] = mutate(tmp1_chromosomes[t])
+
+        # print('after select and corssover number is', len(tmp1_chromosomes))
+
+        if i%10==0:
+            print('now is {} times result.'.format(i))
+            test_chromose =selected[0]
+            print(test_chromose)
+            print('calculate delta is {}'.format(calc_delta(test_chromose)))
+
 
 
     the_best_chromosome = selected[0]
@@ -65,7 +77,7 @@ def init_chromosomes(num=100):
     for i in range(num):
         chromosome = []
         chrom_len = random.randint(20, 60) # the chromosome length is between in 25 and 60
-        while chrom_len%2 == 0:
+        while chrom_len%2 == 0:  # and must be odd number
             chrom_len = random.randint(20, 60)
 
         for k in range(chrom_len):
@@ -94,6 +106,7 @@ def select(chromosomes):
 
     data = []
 
+    #each chromosome was tested with 200 sets of randomly generated data, save average delta
     for i in range(CHECK_CHROMOSOME_NUMBER):
         (x1, y1, x2, y2, x3, y3) = generate_triangle_data(TRIANGLE_POINT_SCOPE)
         triangle = Triangle(Point(x1, y1), Point(x2, y2), Point(x3, y3))
@@ -103,7 +116,6 @@ def select(chromosomes):
 
     chromo_dict = {}
     i = 0
-    #each chromosome was tested with 100 sets of randomly generated data, save average delta
     try:
         for chromosome in chromosomes:
             total_delta = 0
@@ -120,8 +132,14 @@ def select(chromosomes):
                 total_delta += abs(delta)
 
             aver_delta = total_delta/len(data)
+            i=i+1
+            # chromo_dict[aver_delta] = chromosome
+            chromo_dict[chromosome] = aver_delta
+        if len(chromosomes) > 3*len(chromo_dict):
+            print(chromosomes)
+            print(chromo_dict)
+        # print("the dict length is {0}, i is {1}, chrom length is {2}".format(len(chromo_dict), i,len(chromosomes)))
 
-            chromo_dict[aver_delta] = chromosome
     except Exception:
         print("calculate is error: {}".format(chromosome))
         raise
@@ -129,16 +147,19 @@ def select(chromosomes):
     #maybe have some better method to do it, just do now!
     selected = []
 
-    delta, body = zip(*chromo_dict.items())
+    body, delta = zip(*chromo_dict.items())
     body_list = list(body)
     delta_list = list(delta)
-
 
     delta_list.sort(reverse= False)
 
     for m in range(SELECTED_NUMBER):
-        the_chromo = chromo_dict.get(delta_list[m])
-        selected.append(the_chromo)
+        # print(m)
+        # the_chromo = chromo_dict.get(delta_list[m])
+        the_chromo = [k for k,v in chromo_dict.items() if v == delta_list[m]]
+
+        # must insert a string, not a list
+        selected.append(the_chromo[0])
 
     return selected
 
@@ -156,30 +177,111 @@ def crossover(chromosomes):
     '''
 
     generate_chromosomes =[]
+    # print(chromosomes)
     base_number = int(len(chromosomes)/2)
+    # print('base_number is: ', base_number)
     for i in range(base_number):
         tmp_chrom1 = chromosomes[i]
         for k in range(base_number):
             tmp_chrom2 = chromosomes[base_number+k]
-            length1 = int(len(tmp_chrom1)/2)+1
-            length2 = int(len(tmp_chrom2)/2)+1
-            first_half = tmp_chrom1[:length1]
-            second_half = tmp_chrom2[length2:]
-            new_chromosomes = first_half + second_half
-            generate_chromosomes.append(new_chromosomes)
 
+            magic_number = random.random()+1.6
+            # length1 = int(len(tmp_chrom1)/magic_number)+1
+            length1 = int(len(tmp_chrom1) / magic_number) + 1
+            length2 = int(len(tmp_chrom2)/2)
+
+            # print(type(tmp_chrom1))
+            # print(type(tmp_chrom2))
+            # print(len(tmp_chrom1), len(tmp_chrom2))
+            #
+            # print('length1 is :', length1)
+            # print('length2 is :', length2)
+
+            first_half = tmp_chrom1[0:length1]
+            second_half = tmp_chrom2[length2:]
+            # print("tmp_chrom1 is: ", tmp_chrom1)
+            # print("first_half is: ", first_half)
+            #
+            # print("tmp_chrom2 is: ", tmp_chrom2)
+            # print("second_half is: ", second_half)
+
+
+            if first_half[-1] in symbol_list  and second_half[0] in symbol_list:
+                first_half = tmp_chrom1[0:length1-1]
+
+            if second_half[0] in operator_list and first_half[-1] in operator_list:
+                second_half = second_half[1:]
+
+            # print("first_half is: ", first_half)
+            # print("second_half is: ", second_half)
+            # print(first_half)
+            # print(second_half)
+
+            # new_chromosome = first_half.join(second_half)
+            tmp_list = first_half + second_half
+
+
+            # print(type(tmp_list))
+
+            new_chromosome = ''.join(tmp_list)
+
+            # if had same chromsome, mutate it !
+            while new_chromosome in generate_chromosomes:
+                new_chromosome = mutate(new_chromosome)
+
+
+            # check it is right
+            if check_chromosome(new_chromosome):
+                generate_chromosomes.append(new_chromosome)
+            else:
+                print('it is not a right chromosome : {}'.format(new_chromosome))
+                raise
+
+    newlist = set(generate_chromosomes)
+    if len(newlist) < len(generate_chromosomes):
+        print('have {} repeat items!'.format(len(generate_chromosomes)-len(newlist)))
 
     return generate_chromosomes
 
 
+def calc_delta(chromosome):
+
+    data = []
+    total_delta = 0
+
+    #each chromosome was tested with 200 sets of randomly generated data, save average delta
+    for i in range(CHECK_CHROMOSOME_NUMBER):
+        (x1, y1, x2, y2, x3, y3) = generate_triangle_data(50)
+        triangle = Triangle(Point(x1, y1), Point(x2, y2), Point(x3, y3))
+        area = triangle.calc_area()
+        data_item = [x1,y1,x2,y2,x3,y3,area]
+        data.append(data_item)
+
+    for item in data:
+        a = item[0]
+        b = item[1]
+        c = item[2]
+        d = item[3]
+        e = item[4]
+        f = item[5]
+        s = item[6]
+        result = 0.5 * abs(eval(chromosome))
+        delta = (result - s) / s
+        total_delta += abs(delta)
+
+    aver_delta = total_delta / len(data)
+
+    return aver_delta
+
 
 def mutate(chromosome):
     '''
-
+    Perform mutation processing on a given chromosome, and randomly select 1 to 4 symbols to change.
+    letters become to letter, operators become to operators.
     Args:
-        chromosome:
+        chromosome: source chromosome
 
-    Returns:
+    Returns: target chromosome
 
     '''
 
@@ -189,24 +291,47 @@ def mutate(chromosome):
     #Ensue that the generated chromosome can be calculated through  while, try, except
     while new_chromosome == chromosome:
         try:
+            # change number
             k = random.randint(1,4)
             tmp_list = list(new_chromosome)
             for i in range(k):
-                m = random.randint(0, len(chromosome))
-                tmp_list[m] = random.choice(gene_list)
+                #change location
+                m = random.randint(0, len(chromosome)-1)
+                if tmp_list[m]  in symbol_list:
+                    tmp_list[m] = random.choice(symbol_list)
+                elif tmp_list[m] in operator_list:
+                    tmp_list[m] = random.choice(operator_list)
             mutated_chromosome =''.join(tmp_list)
             eval(mutated_chromosome)
             new_chromosome = mutated_chromosome
         except Exception:
             print("cannt calculate the formula: {}".format(mutated_chromosome))
+            # raise
 
     return new_chromosome
 
+
+def check_chromosome(chromosome):
+
+    flag = True
+    # print(len(chromosome))
+    for i in range(len(chromosome)):
+        # print(i)
+        if chromosome[i] not in gene_list:
+            flag = False
+        if i%2 == 0:  # odd number, from zero
+            if chromosome[i] not in symbol_list:
+                flag = False
+        elif i%2 == 1: # even number
+            if chromosome[i] not in operator_list:
+                flag = False
+
+    return flag
 
 
 if __name__ == '__main__':
     # chromosomes = init_chromosomes(60)
     # selected = select(chromosomes)
     # new_chromosomes = crossover(selected)
-    result = genetic_algorithm(1000)
+    result = genetic_algorithm(500)
     print(result)
--- test_GA.py
+++ test_GA.py
@@ -7,37 +7,30 @@
 
 
 import unittest
-from ga import genetic_algorithm, init_chromosomes, select, crossover, mutate
+from ga import genetic_algorithm, init_chromosomes, select, crossover, mutate, check_chromosome
 from shape import generate_triangle_data, Triangle, Point
 
 class TestGA(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.default_formula = genetic_algorithm(1000)
+        # self.default_formula = genetic_algorithm(1000)
+        pass
 
     def tearDown(self) -> None:
-        self.default_formula = None
+        # self.default_formula = None
+        pass
 
 
-    def test_strings(self):
-        '''
-        the formula must be in the gene sets.
-
-        '''
-        gene_set = {'a', 'b', 'c', 'd', 'e', 'f', '+', '-', '*'}
-        for s in self.default_formula:
-            # print(s)
-            # self.assertEqual(True, s in gene_set)
-            self.assertIn(s, gene_set)
 
     def test_calculate(self):
         '''
         the formula can calculate and result is a number.
         '''
         the_formula = genetic_algorithm(100)
-        # result = eval(self.default_formula)
-        #
-        # result1 = eval(the_formula)
+        (a, b, c, d, e, f) = generate_triangle_data(10)
+        print(the_formula)
+        result = 0.5*abs(eval(the_formula))
+        self.assertGreater(result,0)
 
 
     def test_generate_chromosomes(self):
@@ -63,9 +56,7 @@ class TestGA(unittest.TestCase):
         triangle = Triangle(Point(a, b), Point(c, d), Point(d, f))
         s = triangle.calc_area()
 
-        # for k in range(0,len(selected)-1, 5):
-        #     print(k)
-        # print(selected)
+
         start_formula = selected[0]
         end_formula = selected[len(selected)-1]
 
@@ -82,8 +73,8 @@ class TestGA(unittest.TestCase):
 
         self.assertEqual(4, len(gene_chroms))
 
-        self.assertEqual('e*d*e*a*b-b*b-c-e-b+b*f*d*b*c',gene_chroms[0])
-        self.assertEqual('f*b*b-c*f+c*b+b-c-e-b+b*f*d*b*c',gene_chroms[2])
+        self.assertEqual('e*d*e*a*b-b*b-c-e-b+b*f*d*b*c', gene_chroms[0])
+        self.assertEqual('f*b*b-c*f+c*b+b-c-e-b+b*f*d*b*c', gene_chroms[2])
 
     def test_mutate(self):
         begin = 'f*b*b-c*f+c*b+b-c-e-b+b*f*d*b*c'
@@ -93,6 +84,17 @@ class TestGA(unittest.TestCase):
         gene_set = {'a', 'b', 'c', 'd', 'e', 'f', '+', '-', '*'}
         for s in after:
             self.assertIn(s, gene_set)
+
+    def test_check_chromosome(self):
+        right_chroms = ['e*d*e*a*b-b*e*c+f+a+a+c', 'f*b*b-c*f+c*b+d*d+b+e+a+f+e', 'e*e-b-e+d+d+b+e*d-b-c-e-b+b*f*d*b*c',
+                       'e-a+c-a*b*d+c*f-c+a*f-e+d-e*e']
+        for rc in right_chroms:
+            self.assertTrue(check_chromosome(rc))
+
+        error_chroms = ['e*d*e*a*b-b*e*cef+a+a+c', 'f*b*-c*f+c*b+d*d+b+e+a+f+e', 'e*e-bbe+d+d+b+e*d-b-c-e-b+b*f*d*b*c',
+                       'e-a+c-a*+*d+c*f-++a*f-e+d-e*e']
+        for ec in error_chroms:
+            self.assertFalse(check_chromosome(ec))
 
 
 
